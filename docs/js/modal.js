3f4e1c3d9345c3ac98617b0d826bd4fb8a1d4277
k-ohwada <kyousuke.owada@constantine.local>
2018-09-25 12:49:44 +0900

    モーダル更新

--- docs/js/modal.js
+++ docs/js/modal.js
@@ -6,46 +6,57 @@ class Modal {
     this.close = document.querySelector('.modal-close-button');
     this.close_cross = document.querySelector('.modal-close-cross');
     this.modal_text = document.querySelector('.modal-text');
+    this.modal_arrow = [
+      document.querySelector('.modal-left-arrow'),
+      document.querySelector('.modal-right-arrow')
+    ];
+
+    this.index = 0;
 
     this.body = document.body;
 
     this.close_list = [this.overlay, this.close, this.close_cross];
 
+    this.setData();
     this.bind();
   }
 
   bind() {
-    // this.button.forEach(currentValue => {
-    //   currentValue.addEventListener('click', () => {
-    //     this.openModal();
-    //   });
-    // });
-
     this.close_list.forEach(currentValue => {
       currentValue.addEventListener('click', () => {
         this.closeModal();
       });
     });
 
-    [].forEach.call(this.button, modal_text => {
-      modal_text.addEventListener('click', (e) => {
-        this.index = e.currentTarget.dataset.text;
+    [].forEach.call(this.button, button => {
+      button.addEventListener('click', e => {
+        this.modal_text.textContent = e.currentTarget.dataset.text;
 
-        this.modal_text.textContent = this.index;
+        this.index = e.currentTarget.dataset.value;
 
         this.openModal();
       });
     });
+
+    this.modal_arrow[0].addEventListener('click', e => {
+      this.prevText();
+    });
+
+    this.modal_arrow[1].addEventListener('click', e => {
+      this.nextText();
+    });
   }
 
   openModal() {
+    console.log(this.index);
     this.overlay.style.display = 'block';
     this.modal.style.display = 'block';
 
     anime({
       targets: this.modal,
+      easing: 'linear',
       scale: [0, 1],
-      duration: 300
+      duration: 200
     });
     anime({
       targets: this.overlay,
@@ -62,7 +73,8 @@ class Modal {
     anime({
       targets: this.modal,
       scale: 0,
-      duration: 300
+      easing: 'linear',
+      duration: 200
     });
     anime({
       targets: this.overlay,
@@ -84,6 +96,36 @@ class Modal {
     this.overlay.addEventListener('touchmove', e => {
       e.preventDefault();
     });
+  }
+
+  setData() {
+    this.button.forEach((currentValue, index) => {
+      currentValue.dataset.value = index;
+    });
+  }
+
+  nextText() {
+    if (this.index < this.button.length - 1) {
+      this.index++;
+    } else {
+      this.index = 0;
+    }
+
+    this.updateText();
+  }
+
+  prevText() {
+    if (this.index > 0) {
+      this.index--;
+    } else {
+      this.index = this.button.length - 1;
+    }
+
+    this.updateText();
+  }
+
+  updateText() {
+    this.modal_text.textContent = this.button[this.index].dataset.text;
   }
 }
 
